1b375f58b012bcf64e861d32cb499aeb489d2789
ptr <piotr2785.owca@gmail.com>
2023-04-14 19:51:07 +0200

    Countdown timer now refreshes each t-seconds line by erasing previous print. Every scraped exchange rate is now saved in a text file

--- main.py
+++ main.py
@@ -2,40 +2,81 @@ import requests
 import bs4
 import time
 import datetime
-
+import backoff
 
 beginTimeSeconds = 20
 startTime = datetime.datetime.now()
 
-print("Start at: XX:XX:" + str(beginTimeSeconds))
-for a in range(1000):
 
+today = datetime.datetime.now()
+fileName = f'Kurs walut {today.year}-{today.month}-{today.day}.txt'
+file = open(f'{fileName}', "a")
+print(f'Opened file "{fileName}"')
+file.write(f'#======================\n{today.year}-{today.month}-{today.day}\n')
+file.write("# 1.EUR, 2.USD, 3.CHF, 4.GBP\nSell Price, Buy Price\n")
+file.close()
+
+print("Start at: XX:XX:" + str(beginTimeSeconds))
+for a in range(1440):
+    file = open(f'{fileName}', "a")
+    # countdown to start, for sync up with data refresh on website
+    isFirstPrintOnTimer = 0
     while startTime.second != beginTimeSeconds:
         startTime = datetime.datetime.now()
         if(startTime.second>beginTimeSeconds):
-            print("Waiting to start. T-" + str(60+(beginTimeSeconds - startTime.second)))
+            if(isFirstPrintOnTimer == 1):
+                print("\rWaiting to start. T-" + str(60+(beginTimeSeconds - startTime.second)), end=" ")
+            else:
+                print("Waiting to start. T-" + str(60 + (beginTimeSeconds - startTime.second)), end=" ")
+                isFirstPrintOnTimer = 1
         else:
-            print("Waiting to start. T-" + str(beginTimeSeconds - startTime.second))
+            if(isFirstPrintOnTimer == 1):
+                print("\rWaiting to start. T-" + str(beginTimeSeconds - startTime.second), end=" ")
+            else:
+                print("Waiting to start. T-" + str(beginTimeSeconds - startTime.second), end=" ")
+                isFirstPrintOnTimer = 1
+
         if startTime.second != 60:
             time.sleep(0.5)
 
-    print()
-
-    print("Nr: " + str(a+1) + "\nCzas: " + time.asctime())
+    print("\nNr: " + str(a+1) + "\nCzas: " + time.asctime())
 
     now = datetime.datetime.now()
     print("Czas: ", now.year, now.month, now.day, now.hour, now.minute, now.second)
 
-    print("Zakup  Sprzedaż")
-    res = requests.get("https://www.santander.pl/klient-indywidualny/karty-platnosci-i-kantor/kantor-santander")
-    res.raise_for_status()
-    soup = bs4.BeautifulSoup(res.text, "html.parser")
-    sellPrice = soup.find_all("div", {"class": "exchange_office__table-value js-exchange_office__rate-sell-value"})
-    buyPrice = soup.find_all("div", {"class": "exchange_office__table-value js-exchange_office__rate-buy-value"})
+    file.write(f'{str(a+1)} {time.strftime("%H:%M")}\n')
+
+    def backoff_hdlr(details):
+        print("Definite connection error")
+        # close the file when there is no connection to url
+        file.write("Definite connection error")
+        file.close()
+        raise SystemExit(0)
+
+    #decorator for any exception that shows up when there is no internet connection, will try 5 times
+    @backoff.on_exception(
+        backoff.expo,
+        requests.exceptions.RequestException,
+        on_giveup=backoff_hdlr,
+        max_tries=5,
+    )
+    def make_request():
+        return requests.get("https://www.santander.pl/klient-indywidualny/karty-platnosci-i-kantor/kantor-santander")
+
+    # starting the url request
+    print("Sprzedaż  Zakup")
+
+    req = bs4.BeautifulSoup(make_request().text, "html.parser")
+    sellPrice = req.find_all("div", {"class": "exchange_office__table-value js-exchange_office__rate-sell-value"})
+    buyPrice = req.find_all("div", {"class": "exchange_office__table-value js-exchange_office__rate-buy-value"})
 
     for i in range(4):
         print(sellPrice[i].text.strip() + " " + buyPrice[i].text.strip())
+        file.write(f'{sellPrice[i].text.strip()} {buyPrice[i].text.strip()}\n')
 
+    file.close()
+
+    # wait for 1 minut for changes on the website
     time.sleep(0.5)
     now2 = datetime.datetime.now()
     while now2.second != 20:
@@ -43,8 +84,8 @@ for a in range(1000):
         # print(now2.second)
         time.sleep(0.5)
 
-    print()
-
+file.close()
+print(f'File {fileName} is closed')
 
 
 
